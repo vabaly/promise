c267a6388cca36c32211dc4ab136491a7887b7ae
vabaly <love0and0life@gmail.com>
2020-02-12 17:15:19 +0800

    feat: 新增 Promise.all 的学习

--- demo/base.js
+++ demo/base.js
@@ -1,32 +1,62 @@
 const Promise = require('../src/index')
 
-console.log('aa')
-// 生成一个 Promise 实例
-// 此过程大概是在 promise 上挂载一些属性并立即执行 fn 函数
-// 同时立马校验 resolve 的值，不允许 resolve promise 本身
-// 1. 如果 resolve 一个 promsie 对象，则决议状态和其他的不一样
-// 2. 如果 resolve 一个 then 属性是函数的普通对象，那么会立即执行这个 then 函数,
-//    如果这个 then 函数有一个或两个参数，第一个参数将会是 resolve 函数，第二个参数将会是 reject 函数，
-//    最终 new Promise 的决议值是 then 函数中 resolve 或 reject 中的值
-//    这种情况在下面的例子中特别给出来
-// 3. 其他情况，决议状态都是成功，决议值都是里面的值
-const promise = new Promise(function fn (resolve, reject) {
-    console.log('resolve')
-    // 真正的 resolve 值由 then 方法中的 resolve 值决定
-    resolve({
-        then: (resolve, reject) => {
-            resolve('hahaha')
-        }
-    })
-    // 多次 resolve 都会因为 doResolve 中的变量 done 为 true 而不被真正的执行内部的 resolve 函数
+// /**
+//  * --------------------------------------------
+//  *           简单的 Promise 使用 - Start
+//  * --------------------------------------------
+//  */
+
+// // 生成一个 Promise 实例
+// // 此过程大概是在 promise 上挂载一些属性并立即执行 fn 函数
+// // 同时立马校验 resolve 的值，不允许 resolve promise 本身
+// // 1. 如果 resolve 一个 promsie 对象，则决议状态和其他的不一样
+// // 2. 如果 resolve 一个 then 属性是函数的普通对象，那么会立即执行这个 then 函数,
+// //    如果这个 then 函数有一个或两个参数，第一个参数将会是 resolve 函数，第二个参数将会是 reject 函数，
+// //    最终 new Promise 的决议值是 then 函数中 resolve 或 reject 中的值
+// //    这种情况在下面的例子中特别给出来
+// // 3. 其他情况，决议状态都是成功，决议值都是里面的值
+// const promise = new Promise(function fn (resolve, reject) {
+//     console.log('resolve')
+//     // 真正的 resolve 值由 then 方法中的 resolve 值决定
+//     resolve({
+//         then: (resolve, reject) => {
+//             resolve('hahaha')
+//         }
+//     })
+//     // 多次 resolve 都会因为 doResolve 中的变量 done 为 true 而不被真正的执行内部的 resolve 函数
+// })
+
+// promise.then(done => {
+//     console.log('done', done)
+// }, error => {
+//     console.log('error', error)
+// }).catch(error => {
+//     console.log('catch error', error)
+// }).finally(() => {
+//     console.log('finally')
+// })
+
+// /**
+//  * --------------------------------------------
+//  *           简单的 Promise 使用 - end
+//  * --------------------------------------------
+//  */
+
+/**
+ * --------------------------------------------
+ *           Promise.all 使用 - start
+ * --------------------------------------------
+ */
+
+const p1 = Promise.resolve(1)
+const p2 = Promise.resolve(2)
+
+Promise.all([p1, p2]).then(([res1, res2]) => {
+    console.log('res output', res1, res2)
 })
 
-promise.then(done => {
-    console.log('done', done)
-}, error => {
-    console.log('error', error)
-}).catch(error => {
-    console.log('catch error', error)
-}).finally(() => {
-    console.log('finally')
-})
+/**
+ * --------------------------------------------
+ *           Promise.all 使用 - end
+ * --------------------------------------------
+ */
--- src/es6-extensions.js
+++ src/es6-extensions.js
@@ -46,20 +46,42 @@ Promise.resolve = function (value) {
   return valuePromise(value);
 };
 
+/**
+ * Promise all 的实现
+ */
 Promise.all = function (arr) {
+  // 确保是真数组
   var args = Array.prototype.slice.call(arr);
 
+  // Promise.all 方法返回一个 Promise
   return new Promise(function (resolve, reject) {
+    // 当参数是空数组时，决议值是 []
     if (args.length === 0) return resolve([]);
+
+    // 记录还有多少个 promise 未决议
     var remaining = args.length;
+
+    /**
+     * 主要逻辑都在 res 函数中
+     * @param {number} i promise 数组下标
+     * @param {Promise|Object|Function} val 下标对应的 Promise，当然也可以是其他被兼容的对象，暂不考虑
+     */
     function res(i, val) {
       if (val && (typeof val === 'object' || typeof val === 'function')) {
+        // 主要考虑 val 是 promise 对象的情况
         if (val instanceof Promise && val.then === Promise.prototype.then) {
+          // 如果 PromiseA resolve PromiseB，PromiseB 又 resolve PromiseC ...
+          // 那么就一直寻找到最里层的不是 resolve Promise 的一个 Promise 为止
+          // 这个寻找的过程就是通过 _state 这个私有属性来判断，它存储了 Promise resolve 的内容
           while (val._state === 3) {
             val = val._value;
           }
+          // 成功决议时递归调用 res 函数，此时 res 的第二个参数变成了 Promise resolve 的值
           if (val._state === 1) return res(i, val._value);
+          // 决议失败时直接就把 Promise.all 返回的 Promise 给 reject 掉了
           if (val._state === 2) reject(val._value);
+
+          // 还处于 pending 状态的 promise 继续放入下一个微任务队列中去判断
           val.then(function (val) {
             res(i, val);
           }, reject);
@@ -75,11 +97,18 @@ Promise.all = function (arr) {
           }
         }
       }
+
+      // 如果 val 的值是非 Promise 或非特殊对象和函数，都会走到这
+      // 包括 promise 成功决议了之后也会调用 res 走到这
       args[i] = val;
+
+      // 都决议完了之后再把最外层的 Promise 决议了
       if (--remaining === 0) {
         resolve(args);
       }
     }
+
+    // 遍历 promise 数组，并执行 res 函数
     for (var i = 0; i < args.length; i++) {
       res(i, args[i]);
     }
